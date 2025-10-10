2523fa19f24566fa4f54605cb072db8a1699a62a
skyfaqer <37690820+skyfaqer@users.noreply.github.com>
2025-10-09 21:21:01 -0400

    Implement main features

--- js/index.js
+++ js/index.js
@@ -1,8 +1,49 @@
 "use strict";
 
-document.getElementById("next").addEventListener("click", function() {
-  let [ waitingTiles, waitedTiles ] = generateWaitingTiles();
-  console.log(waitingTiles);
-  console.log(waitedTiles);
-  // console.log(getAvailablePairs(waitingTiles));
+import { generateWaitingTiles, getRandomSuit } from './mjutil.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+  renderNewTiles();
 });
+
+document.getElementById('next-puzzle').addEventListener('click', function() {
+  renderNewTiles();
+});
+
+document.getElementById('reveal-waited-tiles').addEventListener('click', function() {
+  document.getElementById('waited-tiles').style.visibility = 'visible';
+  this.disabled = true;
+});
+
+function renderNewTiles() {
+  let [ waitingTiles, waitedTiles ] = getRandomTilesWithSuit();
+  renderTiles('waiting-tiles', waitingTiles);
+  renderTiles('waited-tiles', waitedTiles);
+  document.getElementById('waited-tiles').style.visibility = 'hidden';
+  document.getElementById('reveal-waited-tiles').disabled = false;
+}
+
+function getRandomTilesWithSuit() {
+  let numOfTiles = document.querySelector('input[name="number-of-tiles"]:checked').value;
+  let enableHardMode = document.getElementById('enable-hard-mode').checked;
+  let [ waitingTiles, waitedTiles ] = generateWaitingTiles(numOfTiles, enableHardMode);
+  let suit = getRandomSuit();
+  return [ waitingTiles.map(tile => tile + suit), waitedTiles.map(tile => tile + suit) ];
+}
+
+function renderTiles(containerId, tiles) {
+  const container = document.getElementById(containerId);
+  container.innerHTML = ''; // Clear previous tiles
+
+  // const labelElem = document.createElement('span');
+  // labelElem.textContent = label;
+  // container.appendChild(labelElem);
+
+  tiles.forEach(tile => {
+    const img = document.createElement('img');
+    img.src = `./images/tiles/${tile}.png`;
+    img.className = 'tile-img';
+    img.alt = tile;
+    container.appendChild(img);
+  });
+}
