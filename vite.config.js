f8e2a92d20b0acbb82c1a5f422577a4f6c1405c3
tar0cc0 <chengguoyao@hotmail.com>
2025-11-12 22:04:54 -0500

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-export default {
-  root: 'src',
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const __root = 'src'
+
+export default defineConfig({
+  root: __root,
+  base: '/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
-  },
-  base: '/'
-}
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, `${__root}/index.html`),
+        privacy: resolve(__dirname, `${__root}/privacy.html`)
+      }
+    }
+  }
+})
